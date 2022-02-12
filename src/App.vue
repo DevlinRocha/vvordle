cfaa59a65957687cb29b5cc8c1bac732c4ab16a7
Devlin <devlincodes@gmail.com>
2022-02-11 22:59:19 -0800

    Style App

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,22 @@
 <template></template>
 
 <style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  background-color: hsl(240, 3%, 7%);
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
