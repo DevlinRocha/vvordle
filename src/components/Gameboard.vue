96679f79dce4483c663b5b9f72b598b1c781ba67
Devlin <devlincodes@gmail.com>
2022-02-12 19:30:48 -0800

    Create Gameboard component

---
+++ src/components/Gameboard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="gameboard">
+    <div class="tile" v-for="tile in 30"></div>
+  </div>
+</template>
+
+<style scoped>
+.gameboard {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+  grid-template-columns: repeat(5, 4em);
+  grid-template-rows: repeat(6, 4em);
+  gap: 0.25em;
+  margin-bottom: 1em;
+}
+
+.tile {
+  font-size: 2em;
+  color: white;
+  border: 0.05em solid hsl(240, 2%, 23%);
+  width: 100%;
+  height: 100%;
+  text-transform: uppercase;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+}
+</style>
