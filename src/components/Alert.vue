e526e5f3bf1d4f830c639e9568de345802be671d
Devlin <devlincodes@gmail.com>
2022-05-15 12:52:19 -0700

    Create alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({
+  alertMessage: String,
+});
+</script>
+
+<template>
+  <div class="alert-container">
+    <span class="alert">{{ alertMessage }}</span>
+  </div>
+</template>
+
+<style scoped>
+.alert-container {
+  position: fixed;
+  top: 10vh;
+}
+
+.alert {
+  pointer-events: none;
+  background-color: hsl(204, 7%, 85%);
+  padding: 0.75rem;
+  border-radius: 0.25rem;
+  margin-bottom: 0.5rem;
+}
+
+.alert:last-child {
+  margin-bottom: 0;
+}
+</style>
