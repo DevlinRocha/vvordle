8875f8ba27d7452b66af51946d27a843bf38e19a
Devlin <devlincodes@gmail.com>
2022-07-10 15:30:46 -0700

    Create Results component

---
+++ src/components/Results.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const emit = defineEmits(["closeResults"]);
+
+function close() {
+  emit("closeResults");
+}
+</script>
+
+<template>
+  <div @click="close" class="backdrop">
+    <div class="wrapper"><button>SHARE</button></div>
+  </div>
+</template>
+
+<style scoped>
+.backdrop {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  max-width: 500px;
+  max-height: 100%;
+  padding: 1rem;
+  border: 1px solid #1a1a1b;
+  border-radius: 0.5rem;
+  background-color: #121213;
+  box-shadow: 0 4px 23px 0 rgb(0 0 0 / 20%);
+}
+
+button {
+  height: 52px;
+  width: 80%;
+  border: none;
+  border-radius: 0.25rem;
+  color: #fff;
+  background-color: hsl(115, 29%, 43%);
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  user-select: none;
+}
+</style>
