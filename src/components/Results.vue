a9e63d3ef30f8509cadee60eb30436da8e45726c
Devlin <devlincodes@gmail.com>
2022-07-23 02:34:32 -0700

    Add countdown to Results modal

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+
 const emit = defineEmits(["share", "close"]);
+
+const countdown = ref("00:00:00");
 
 function share() {
   emit("share");
@@ -8,12 +12,31 @@ function share() {
 function close() {
   emit("close");
 }
+
+function updateCountdown() {
+  const today = new Date();
+  countdown.value = `${String(23 - today.getHours()).padStart(2, "0")}:${String(
+    59 - today.getMinutes()
+  ).padStart(2, "0")}:${String(59 - today.getSeconds()).padStart(2, "0")}`;
+}
+
+onMounted(() => {
+  updateCountdown();
+
+  setInterval(() => {
+    updateCountdown();
+  }, 1000);
+});
 </script>
 
 <template>
   <div @click="close" class="backdrop">
     <div @click.stop class="wrapper">
-      <button @click.stop="share">SHARE</button>
+      <section>
+        <div>NEXT VVORDLE</div>
+        <time>{{ countdown }}</time>
+      </section>
+      <button @click.stop="share">Share</button>
     </div>
   </div>
 </template>
@@ -31,27 +54,45 @@ function close() {
 
 .wrapper {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   width: 90%;
   max-width: 500px;
   max-height: 100%;
   padding: 1rem;
   border: 1px solid #1a1a1b;
   border-radius: 0.5rem;
+  color: #fff;
   background-color: #121213;
   box-shadow: 0 4px 23px 0 rgb(0 0 0 / 20%);
+}
+
+section {
+  text-align: center;
+}
+
+section div {
+  font-weight: 700;
+  letter-spacing: 1px;
+  margin: 0.75rem 0;
+}
+
+section time {
+  font-size: 2rem;
+  letter-spacing: 1px;
 }
 
 button {
   height: 52px;
   width: 80%;
   border: none;
-  border-radius: 0.25rem;
-  color: #fff;
+  border-radius: 6.25rem;
+  color: inherit;
   background-color: hsl(115, 29%, 43%);
   font-size: 20px;
   font-weight: bold;
   cursor: pointer;
   user-select: none;
+  margin: 0.75rem 0;
 }
 </style>
