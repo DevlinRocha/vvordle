2684fd671ce1a23652001f2bcff8a8d22b45fdcf
Devlin <devlincodes@gmail.com>
2022-09-05 02:30:55 -0700

    Don't show alert message when using Web Share API

--- src/App.vue
+++ src/App.vue
@@ -145,13 +145,12 @@ function closeResults() {
 }
 
 function shareResults() {
-  alert.value.showAlert("Copied results to clipboard", 2500);
-
   const results = gameboard.value.getResults();
 
-  !!navigator.share
-    ? navigator.share({ text: results })
-    : navigator.clipboard.writeText(results);
+  if (!!navigator.share) return navigator.share({ text: results.join("") });
+
+  alert.value.showAlert("Copied results to clipboard", 2500);
+  navigator.clipboard.writeText(results);
 }
 
 function isMobile() {
